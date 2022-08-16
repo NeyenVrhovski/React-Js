5a5b36f3ce37ce71913d0691604b86335addbcff
Neyen Vrhovski <neyen.vrhovski@itrocksweb.com>
2022-08-16 10:49:06 -0300

    add-dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We are working for you. This page will be available soon.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.mercadolibre.com.ar/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          In the meantime, you can buy things in MercadoLibre
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
