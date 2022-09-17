972b0130699157d73a05bdb3babe2b264bd66eb5
Neyen Vrhovski <neyen.vrhovski@itrocksweb.com>
2022-09-17 20:29:03 -0300

    cart-view

--- src/components/atoms/CarWidget/index.jsx
+++ src/components/atoms/CarWidget/index.jsx
@@ -1,16 +1,19 @@
 import { useContext } from "react";
-import imgUrl from "../../../assets/icons/shoppingCart.png";
+import { Link } from "react-router-dom";
 import { CartContext } from "../../molecules/CartContext";
-import './CarWidget.scss'
+import './CarWidget.scss';
+import { BsCartFill } from "react-icons/bs";
 
 const CarWidget = () => {
 
     const { cartQuantity } = useContext(CartContext);
 
     return (
-        <div className="mainCartContainer">
-            <img src={imgUrl}></img>
-            <span>{cartQuantity}</span>
+        <div style={cartQuantity <= 0 ? {visibility: 'hidden'} : {}} className="mainCartContainer">
+            <Link to={'/cart'}>
+                <BsCartFill style={{width: 25, height: 25}}/>
+                <span>{cartQuantity}</span>
+            </Link>
         </div>
     )
 }
